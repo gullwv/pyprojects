3f54b7507472eb0a33610b0906b41f52248c577c
gullwv <21lundga@wtahs.com>
2018-11-13 11:05:37 -0500

    added python project files

---
+++ henlo.py
@@ -0,0 +1,10 @@
+# henlo.py
+# created on November 13, 2018
+# by Gull
+
+def hello():
+	fren = str(input("what is your name, friend? ")) #get a name for variable fren
+	print("hello,", fren, ", and welcome to henlo.py!") #say hello to fren
+	print("how you doing today?")
+
+hello() #hello!
