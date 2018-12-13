12e0a1f06bdf7aa7248ffed4b8736b356f5dbba3
gullwv <21lundga@wtahs.com>
2018-12-13 08:33:59 -0500

    fixed input spacing

--- challenges/1.2/challenge2.py
+++ challenges/1.2/challenge2.py
@@ -3,8 +3,8 @@
 # greet the user after they input their name
 
 def main():
-	first=str(input("What is your first name?"))
-	last=str(input("What is your last name?"))
+	first=str(input("What is your first name? "))
+	last=str(input("What is your last name? "))
 	print("Hello,", first, last + "!")
 	
 main()
